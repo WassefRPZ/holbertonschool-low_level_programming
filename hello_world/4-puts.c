ac3b09ed4e0d49a45cbf1ae641b91d1be4ed0642
WassefRPZ <10422@holbertonstudents.com>
2025-02-17 10:19:47 +0000

    task 4 C test 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
-# include  <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include<stdio.h>
 
-int  main ( void )
- { 
- puts ( "\"Programming is like building a multilingual puzzle" ); 
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 
-  return ( 0 ); 
+return (0);
 }
 
