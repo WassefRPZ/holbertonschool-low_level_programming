bb02432d2c26ca963e6a01e55f783100848c5be0
WassefRPZ <10422@holbertonstudents.com>
2025-02-18 15:34:12 +0000

    add file 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
 	int i;
-	i = 97;
 
+	i = 97;
 	while (i <= 122)
 	{
-	putchar(i);
-	i++;
+		putchar(i);
+		i++;
 	}
 	putchar('\n');
 	return (0);
