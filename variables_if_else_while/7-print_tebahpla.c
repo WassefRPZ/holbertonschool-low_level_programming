348866db8d8fba75001ef86b7d435d141b579577
WassefRPZ <10422@holbertonstudents.com>
2025-02-19 20:26:00 +0000

    task 7 Smile in the mirror C

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+
+{
+	int i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
