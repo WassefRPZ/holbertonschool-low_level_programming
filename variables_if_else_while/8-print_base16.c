676f591305834767b9268dd69636a5854a97415e
WassefRPZ <10422@holbertonstudents.com>
2025-02-20 20:19:52 +0000

    task 8  Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int r;
+
+		i = 48;
+		while (i <= 57)
+		{
+			putchar(i);
+			i++;
+
+		}
+
+		r = 65;
+		while (r <= 70)
+		{
+			putchar(r);
+			r++;
+
+		}
+		putchar('\n');
+		return (0);
+}
