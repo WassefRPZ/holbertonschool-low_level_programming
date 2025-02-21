5cae532d8debfb1d045560f8c0f5e70e912fb053
WassefRPZ <10422@holbertonstudents.com>
2025-02-21 19:57:19 +0000

    task 0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+
+    putchar('\n');
+    return (0);
+}
