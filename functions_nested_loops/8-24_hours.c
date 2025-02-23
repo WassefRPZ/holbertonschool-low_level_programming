e171e60ab2fe3af9f75496a8b2e31925686d2009
WassefRPZ <10422@holbertonstudents.com>
2025-02-23 22:41:31 +0000

    task 8 test 8888

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,25 +11,24 @@
  */
 void jack_bauer(void)
 {
-void jack_bauer(void)
-{
-	int i = 0, j = 0;
+        int i, j;
 
-	while (i < 24)
-	{
-	j = 0;
-	while (j < 60)
+        i = 0;
+
+        while (i < 24)
         {
-
-	_putchar((i / 10) + '0');
-	_putchar((i % 10) + '0');
-	_putchar(':');
-	_putchar((j / 10) + '0');
-	_putchar((j % 10) + '0');
-	_putchar('\n');
-	j++;
-	}
-	i++;
-	}
+                j = 0;
+                while (j < 60)
+                {
+                        _putchar((i / 10) + '0');
+                        _putchar((i % 10) + '0');
+                        _putchar(':');
+                        _putchar((j / 10) + '0');
+                        _putchar((j % 10) + '0');
+                        _putchar('\n');
+                        j++;
+                }
+                i++;
+        }
 }
 
