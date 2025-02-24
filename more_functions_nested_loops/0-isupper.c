c6ba6760c17300394404c8c8dd5eb2e52b729cb8
WassefRPZ <10422@holbertonstudents.com>
2025-02-24 09:16:19 +0000

    task 0 isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character
+ *
+ * @c: The character to check.
+ *
+ * Description: This function takes a character as input and checks
+ *
+ *
+ *
+ * Return: 1, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
