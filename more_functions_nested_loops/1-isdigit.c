fd44dc857e1c04906d7e72ca004ea18fb9541473
WassefRPZ <10422@holbertonstudents.com>
2025-02-24 09:30:55 +0000

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks 0 to 9
+ *
+ * @c: The character to num.
+ *
+ * Description: This function takes a character as input and checks
+ *
+ *
+ *
+ * Return: 1, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ more_functions_nested_loops/1-isdigit.cy
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks 0 9
+ *
+ * @c: The character to check.
+ *
+ *
+ *
+ *
+ *
+ * Return: 1, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
