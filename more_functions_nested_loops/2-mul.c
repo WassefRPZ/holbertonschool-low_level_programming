c271bd25c4e9760dd1b265b7117961be289ab110
WassefRPZ <10422@holbertonstudents.com>
2025-02-24 10:13:47 +0000

    task 2 Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * @b:dfsdfd
+ *
+ * @a: gfdsgdfgg
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
