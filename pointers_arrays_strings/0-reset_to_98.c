cc24678a952c4274f13a8767dbc90b4ca13534bb
WassefRPZ <10422@holbertonstudents.com>
2025-02-27 22:31:31 +0000

    0. 98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Modifie la valeur pointée par le pointeur à 98
+ * @n: Un pointeur vers un entier
+ *
+ * Cette fonction prend en paramètre un pointeur vers un entier et modifie
+ * la valeur de cet entier à 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
