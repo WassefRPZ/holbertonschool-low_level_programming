d118f0e25a585f8d574568a55bfa9a0df4aedaab
WassefRPZ <10422@holbertonstudents.com>
2025-03-02 20:05:35 +0000

    task 0 test 2

--- pointers_arrays_strings2/0-strcat.c
+++ pointers_arrays_strings2/0-strcat.c
@@ -11,18 +11,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, c = 0;
+	int i = 0;
+	int j = 0;
 
 
 	while (dest[i] != '\0')
 	i++;
 
 
-	while (src[c] != '\0')
+	while (src[j] != '\0')
 	{
-		dest[i] = src[c];
+		dest[i] = src[j];
 		i++;
-		c++;
+		j++;
 	}
 
 
