2ea4c7662d2174c8704fa9a1a66944cd96e22730
WassefRPZ <10422@holbertonstudents.com>
2025-03-04 16:00:14 +0000

    task 5 sqrt_recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - function that returns the value of x and y
+ * @x : x represents the base
+ * @y : y represents the exponent
+ * Return: return the computed result
+ */
+int _sqrt_recursion(int n)
+
+
