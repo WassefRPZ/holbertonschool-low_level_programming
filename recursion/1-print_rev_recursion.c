ab889252fe1a019ec2e896250369fda449a9b6a6
WassefRPZ <10422@holbertonstudents.com>
2025-03-04 10:15:42 +0000

    task 1 _print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Affiche une chaine de caracte.
+ * @s: La chaine de caracte a  afficher.
+ *
+ * La fonction affiche la chaîne de caractères pointée par s,
+ * caractère par caractère, en appelant la fonction récursivement.
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	 _print_rev_recursion(s + 1);
+	_putchar(*s);
+}
