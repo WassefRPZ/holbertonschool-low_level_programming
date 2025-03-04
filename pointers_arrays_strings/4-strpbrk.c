fd86985d3731b0d9b0bb1b3e3c1b58c4c6b1064f
WassefRPZ <10422@holbertonstudents.com>
2025-03-04 08:38:01 +0000

    task 4 strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+char *_strpbrk(char *s, char *accept)
+{
+
+int i = 0;
+int j;
+
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while(accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			return ( s+i);
+			j++;
+		}
+		i++;
+	}
+
+	return (NULL);
+
+}
+
