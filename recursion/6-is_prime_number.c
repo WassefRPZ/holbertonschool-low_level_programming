9554c24dba158c91e8c41df1514f36a7994d7125
WassefRPZ <10422@holbertonstudents.com>
2025-03-05 10:23:58 +0000

    task 6 test 3

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _prime_manno - Help function
+* _prime_helper - Help function
 * @n: Number we test
-* @prime:  Variable diviser
+* @number:  Variable diviser
 * Return: Return 1 if the input integer is a prime number, otherwise return 0
 */
-int _prime_mano(int n, int prime)
+int _prime_helper(int n, int number)
 {
-	if (n % prime == 0)
-	return (0);
-	if (prime * prime > n)
-	return (1);
-	return (_prime_mano(n, prime + 1));
+if (n % number == 0)
+return (0);
+if (number * number > n)
+return (1);
+return (_prime_helper(n, number + 1));
 }
 /**
-* is_prime_mano - Determines if an integer is a prime mano.
+* is_prime_number - Determines if an integer is a prime number.
 * @n: Number we test
-* Return: Returns 1 if the input integer is a prime mano, otherwise return 0
+* Return: Returns 1 if the input integer is a prime number, otherwise return 0
 */
-
 int is_prime_number(int n)
 {
-	if (n < 2)
-	return (0);
-	return (_prime_mano(n, 2));
+if (n < 2)
+return (0);
+return (_prime_helper(n, 2));
 }
