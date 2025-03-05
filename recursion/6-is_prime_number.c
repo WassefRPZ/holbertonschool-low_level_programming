5f3105090850116647a794cdb2aa6c24b70dfcf8
WassefRPZ <10422@holbertonstudents.com>
2025-03-05 10:21:06 +0000

    task 6 test 2

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,20 +8,21 @@
 */
 int _prime_mano(int n, int prime)
 {
-if (n % prime == 0)
-return (0);
-if (prime * prime > n)
-return (1);
-return (_prime_mano(n, prime + 1));
+	if (n % prime == 0)
+	return (0);
+	if (prime * prime > n)
+	return (1);
+	return (_prime_mano(n, prime + 1));
 }
 /**
 * is_prime_mano - Determines if an integer is a prime mano.
 * @n: Number we test
 * Return: Returns 1 if the input integer is a prime mano, otherwise return 0
 */
+
 int is_prime_number(int n)
 {
-if (n < 2)
-return (0);
-return (_prime_mano(n, 2));
+	if (n < 2)
+	return (0);
+	return (_prime_mano(n, 2));
 }
