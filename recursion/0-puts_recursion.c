a6a82a279a0bc8f812ccb926ec3f50c3a4d89fcc
WassefRPZ <10422@holbertonstudents.com>
2025-03-05 10:11:18 +0000

    task 0 test 2

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,14 +9,16 @@
  */
 void _puts_recursion(char *s)
 {
+	int i = 0;
 
 	if (*s == '\0')
-	{
-		_putchar('\n');
+  	{
+		_putchar ('\n');
 		return;
 	}
-	_putchar(*s);
+	_putchar(s[i]);
 
-	 _puts_recursion(s + 1);
+	_puts_recursion(s + 1);
+
 
 }
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _prime_manno - Help function
+* @n: Number we test
+* @prime:  Variable diviser
+* Return: Return 1 if the input integer is a prime number, otherwise return 0
+*/
+int _prime_mano(int n, int prime)
+{
+if (n % prime == 0)
+return (0);
+if (prime * prime > n)
+return (1);
+return (_prime_mano(n, prime + 1));
+}
+/**
+* is_prime_mano - Determines if an integer is a prime mano.
+* @n: Number we test
+* Return: Returns 1 if the input integer is a prime mano, otherwise return 0
+*/
+int is_prime_number(int n)
+{
+if (n < 2)
+return (0);
+return (_prime_mano(n, 2));
+}
