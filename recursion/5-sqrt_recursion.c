369231b7f6a40ace65244df406b89beff94930f7
WassefRPZ <10422@holbertonstudents.com>
2025-03-05 09:56:36 +0000

    task 5 sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _pow_recursion - function that returns the value of x and y
- * @x : x represents the base
- * @y : y represents the exponent
- * Return: return the computed result
- */
+*_sqrt_mano - mano function for calculating the square root
+* @n: the number to find the square root of
+* @racine: the current guess for the square root
+* Return: The square root of n if it exists, otherwise -1.
+*/
+int _sqrt_mano(int n, int racine)
+{
+if (racine * racine > n)
+return (-1);
+if (racine * racine == n)
+return (racine);
+return (_sqrt_mano(n, racine + 1));
+}
+/**
+*_sqrt_recursion - Computes the square root of a non-negative integer
+* @n: the number to find the square root of
+* Return: the square root of n if it exists, otherwise -1
+*/
 int _sqrt_recursion(int n)
-
-
+{
+if (n < 0)
+return (-1);
+return (_sqrt_mano(n, 1));
+}
