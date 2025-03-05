117c548dec815419ac4a748fba011a916a84777c
WassefRPZ <10422@holbertonstudents.com>
2025-03-05 21:05:35 +0000

    task 1 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Affiche le nombre d'arguments passes au programme
+ * @argc: Le nombre d'arguments passes (y compris le nom du programme)
+ * @argv: Tableau de cha  nes contenant les arguments passes au programme
+ *
+ * Return: Retourne 0 (indique une execution reussie)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
