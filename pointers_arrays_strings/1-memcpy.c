407fe847671940003671518770b4be97445c902b
WassefRPZ <10422@holbertonstudents.com>
2025-03-07 16:47:47 +0000

    task 1 memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _memcpy - Copie une zone de memoire d'une adresse source vers une adresse.
+ * @dest: Pointeur vers la destination où les données seront copies.
+ * @src: Pointeur vers la source d'où les données seront copies.
+ * @n: Nombre de bytes à copier de `src` vers `dest`.
+ *
+ * Return: Retourne le pointeur vers la destination `dest` après la copie.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
