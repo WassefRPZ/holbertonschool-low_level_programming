5ccad4e9da023ebdbd0c394d6e29b7fe83e530d0
WassefRPZ <10422@holbertonstudents.com>
2025-03-14 21:17:10 +0000

    task 0 structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - structure représentant un chien
+ *
+ * @name: Le nom du chien (une chaîne de caractères)
+ * @age: L'âge du chien (un nombre réel)
+ * @owner: Le nom du propriétaire du chien (une chaîne de caractères)
+ */
+struct dog
+	{
+	char *name;
+	float age;
+	char *owner;
+	}
+#endif
