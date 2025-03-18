88a484c232513330f0f1bc6b86c28eb53a0c701d
WassefRPZ <10422@holbertonstudents.com>
2025-03-18 14:54:04 +0100

    feat: enhance print_name function with detailed documentation and improved formatting

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"function_pointers.h"
+void array_iterator(int *array, size_t size, void (*action)(int))
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,2 +1,7 @@
 
 void print_name(char *name, void (*f)(char *));
+Traduis et explique moi de façon clair, concis, et simple la consigne en prenant en compte les prérequis et contraintes générales que l'on a vu plus tôt et pense à me montrer juste le résultat attendu pas la solution de l'exercice.
+N'oublies que lorsque je te fais une proposition tu es sensé me dire si j'ai réussi ou non, si non, tu dois me dire le nombre d'éléments corrects (me les signaler), le nombre d'éléments incorrects (me les signaler), et le nombre d'éléments manquants.
+Tu peux si tu le désir m'aiguiller avec des indices.
+IMPORTANT NE JAMAIS OUBLIER CETTE PARTIE:
+Si la tache que je te présente à des similitudes (un code similaire à quelques détails prêts) avec la précédente ou une que j'aurais vu quelques taches en arrière n'hésites pas à me le signaler, en faisant référence à la tache la plus pertinente en question et m'indiquer si je dois ajuster mon code en fonction.
