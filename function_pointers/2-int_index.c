2c3e610363c538f75570f0fa4e6d25d01c86bb2d
WassefRPZ <10422@holbertonstudents.com>
2025-03-19 18:57:13 +0100

    feat: add documentation for int_index function and improve formatting

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"function_pointers.h"
-
+/**
+ * int_index - Trouve l'index du premier élément qui satisfait cmp
+ * @array: Tableau d'entiers
+ * @size: Taille du tableau
+ * @cmp: Fonction de comparaison
+ *
+ * Return: Index du premier élément correspondant, ou -1 si aucun ou invalide
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -12,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-		return(i);
+		return (i);
 	}
 return (-1);
 
