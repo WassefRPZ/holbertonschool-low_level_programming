efddfcb84b5c81ee5d40c9cb746d772fb1170d54
WassefRPZ <10422@holbertonstudents.com>
2025-03-19 15:32:43 +0100

    feat: add array_iterator function with documentation and implementation

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include"function_pointers.h"
+/**
+ * array_iterator - Applique une fonction sur chaque élément d'un tableau
+ * @array: Tableau d'entiers
+ * @size: Taille du tableau
+ * @action: Fonction à appliquer
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+	for (i = 0; i < size; i++)
+	action(array[i]);
+	}
+}
