9329afe9fd99ac3e82174ae972f902fd8b7e6d4e
WassefRPZ <10422@holbertonstudents.com>
2025-03-21 23:58:48 +0100

    fix: correct formatting and remove duplicate function declarations in variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,24 +3,31 @@
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
 
 /**
- * struct printer - Structure for printing different types
- * @c: The format specifier
- * @f: The function pointer to print the type
- */
+* struct printer - Structure for printing different types
+* @c: The format specifier
+* @f: The function pointer to print the type
+*/
 	typedef struct printer
 	{
 		char *c;
+
 		void (*f)(va_list);
 	} print_t;
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
 
 /* Fonctions d'impression pour chaque type */
