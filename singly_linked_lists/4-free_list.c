1ad29ba93f6f0f77165eb5a5f80544856af4ac13
WassefRPZ <10422@holbertonstudents.com>
2025-04-02 15:01:08 +0200

    feat: add free_list function to deallocate memory for linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+
+
+while (head != NULL)
+{
+tmp = head;
+head = head->next;
+free(tmp->str);
+free(tmp);
+}
+
+}
+
+
+
+
