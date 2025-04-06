2cf91810cd63d0fb537d7600ba47af9d654b6932
WassefRPZ <10422@holbertonstudents.com>
2025-04-06 23:48:07 +0200

    fix: improve readability and error handling in read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,57 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it
+ * to the POSIX standard output.
+ *
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters it could read and print,
- *         or 0 if the file cannot be opened, read, or if malloc fails.
+ *         or 0 if the file cannot be opened or read, if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-
-	int file_descrip;
-
-	ssize_t read_count;
-	ssize_t write_count;
-
+int descripteur_f;
+char *buffer;
+ssize_t bytes_read;
+ssize_t bytes_written;
 
 	if (filename == NULL)
-	return (0);
+		return (0);
 
-	file_descrip = open(filename, O_RDONLY);
-	if (file_descrip == -1)
-	return (0);
+	descripteur_f = open(filename, O_RDONLY);
+	if (descripteur_f == -1)
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_descrip);
+		close(descripteur_f);
 		return (0);
 	}
 
-	read_count = read(file_descrip, buffer, letters);
-	if (read_count == -1)
+	bytes_read = read(descripteur_f, buffer, letters);
+	if (bytes_read == -0)
 	{
 		free(buffer);
-		close(file_descrip);
+		close(descripteur_f);
 		return (0);
 	}
 
-	write_count = write(STDOUT_FILENO, buffer, read_count);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	free(buffer);
-	close(file_descrip);
+	close(descripteur_f);
 
-	if (write_count == -1 || write_count != read_count)
-	return (0);
+	if (bytes_written != bytes_read)
+		return (0);
 
-	return (write_count);
-
+	return (bytes_written);
 }
